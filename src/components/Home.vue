0f69db7719ec1bef8da827a8abeb55c8950608a3
Herve07h22 <herve@07h22.com>
2018-08-26 09:24:41 +0200

    with redirect

---
+++ src/components/Home.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <section class="hero is-info is-large">
+  <div class="hero-head">
+    <nav class="navbar">
+      <div class="container">
+        <div class="navbar-brand">
+          <a class="navbar-item">
+            <img src="https://bulma.io/images/bulma-type-white.png" alt="Logo">
+          </a>
+          <span class="navbar-burger burger" data-target="navbarMenuHeroB">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+        </div>
+        <div id="navbarMenuHeroB" class="navbar-menu">
+          <div class="navbar-end">
+            <a class="navbar-item is-active">
+              Home
+            </a>
+            <a class="navbar-item">
+              Examples
+            </a>
+            <a class="navbar-item">
+              Documentation
+            </a>
+            <span class="navbar-item">
+              <a class="button is-info is-inverted">
+                <span class="icon">
+                  <i class="fab fa-github"></i>
+                </span>
+                <span>DIY on netlify</span>
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+
+  <div class="hero-body">
+      <div class="columns">
+        <div class="column has-text-centered">
+          <p class="title">
+          Instantly turn your tweeter feed into a personnal blog
+          </p>
+          <p class="subtitle">
+          Subtitle
+          </p>
+        </div>
+        <div class="column">
+          <p class="title">
+          Title
+        </p>
+        <p class="subtitle">
+          Subtitle
+        </p>
+        </div>  
+    </div>
+  </div>
+</section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
