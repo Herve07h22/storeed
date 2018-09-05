58d500b923134b553ece09d2a0e74e603e8dcd5d
Herve07h22 <herve@07h22.com>
2018-09-05 22:26:24 +0200

    added legals

---
+++ src/components/Legals.vue
@@ -0,0 +1,139 @@
+<template>
+    <div class="modal" :class="{ 'is-active' : isVisible} ">
+    <div class="modal-background"></div>
+    <div class="modal-content">
+        <div class="content">
+<h1 class="title"> Terms of Service</h1>
+<h2 class="subtitle">1. Terms</h2>
+<p>By accessing the website at https://storeed.me, you are agreeing to be bound by these terms of service, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this website are protected by applicable copyright and trademark law.</p>
+<h2 class="subtitle">2. Use License</h2>
+<p>a.	Permission is granted to temporarily download one copy of the materials (information or software) on Storeed's website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:</p> 
+<ul>
+    <li>i.	modify or copy the materials;</li>
+    <li>ii.	use the materials for any commercial purpose, or for any public display (commercial or non-commercial);</li> 
+    <li>iii.	attempt to decompile or reverse engineer any software contained on Storeed's website; </li>
+    <li>iv.	remove any copyright or other proprietary notations from the materials; or</li>
+    <li>v.	transfer the materials to another person or "mirror" the materials on any other server.</li>
+</ul>
+<p> 
+b.	This license shall automatically terminate if you violate any of these restrictions and may be terminated by Storeed at any time. Upon terminating your viewing of these materials or upon the termination of this license, you must destroy any downloaded materials in your possession whether in electronic or printed format.</p>
+<h2 class="subtitle">3. Disclaimer</h2>
+<p>a.	The materials on Storeed's website are provided on an 'as is' basis. Storeed makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties including, without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights. </p>
+<p>b.	Further, Storeed does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its website or otherwise relating to such materials or on any sites linked to this site. </p>
+<h2 class="subtitle">4. Limitations</h2>
+<p>In no event shall Storeed or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption) arising out of the use or inability to use the materials on Storeed's website, even if Storeed or a Storeed authorized representative has been notified orally or in writing of the possibility of such damage.</p>
+<p> Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.</p>
+<h2 class="subtitle">5. Accuracy of materials</h2>
+The materials appearing on Storeed's website could include technical, typographical, or photographic errors. Storeed does not warrant that any of the materials on its website are accurate, complete or current. Storeed may make changes to the materials contained on its website at any time without notice. However Storeed does not make any commitment to update the materials.
+<h2 class="subtitle">6. Links</h2>
+<p>Storeed has not reviewed all of the sites linked to its website and is not responsible for the contents of any such linked site. 
+The inclusion of any link does not imply endorsement by Storeed of the site. 
+Use of any such linked website is at the user's own risk.</p>
+<h2 class="subtitle">7. Modifications</h2>
+<p>Storeed may revise these terms of service for its website at any time without notice. 
+By using this website you are agreeing to be bound by the then current version of these terms of service.</p>
+<h2 class="subtitle">8. Governing Law</h2>
+<p>These terms and conditions are governed by and construed in accordance with the laws of France and you irrevocably submit to the exclusive jurisdiction of the courts in that State or location.</p>
+<h1 class="title">Privacy Policy</h1>
+<p>Your privacy is important to us. It is Storeed's policy to respect your privacy regarding any information we may collect from you across our website, https://storeed.me, and other sites we own and operate.</p>
+<h2 class="subtitle">Information we collect</h2>
+<p><b>Log data</b></p>
+<p>When you visit our website, our servers may automatically log the standard data provided by your web browser. This data is considered “non-identifying information”, as it does not personally identify you on its own. It may include your computer’s Internet Protocol (IP) address, your browser type and version, the pages you visit, the time and date of your visit, the time spent on each page, and other details.
+We may also collect data about the device you are using to access our website. This data may include the device type, operating system, unique device identifiers, device settings, and geo-location data. What we collect can depend on the individual settings of your device and software. We recommend checking the policies of your device manufacturer or software provider to learn what information they make available to us.</p>
+<p><b>Personal information</b></p>
+<p>We may ask for personal information, such as your:
+<ul>
+<li>Name</li>
+<li>Email</li>
+<li>Twitter identifier</li>
+<li>Phone/mobile number</li>
+<li>Payment information</li>
+</ul></p>
+<p>This data is considered “identifying information”, as it can personally identify you.
+We only request this information when you interact with our services—like signing up for an account, making use of our platform features, paying for access, or subscribing to our newsletter. We only use collected personal information in the context of providing or improving these services.</p>
+<p><b>Business data</b></p>
+<p>Business data refers to data that accumulates over the normal course of operation on our platform. This may include transaction records, stored files, user profiles, analytics data and other metrics, as well as other types of information, created or generated, as users interact with our services.</p>
+<h2 class="subtitle">How we collect information</h2>
+<p>We collect information by fair and lawful means, with your knowledge and consent. We also let you know why we’re collecting it and how it will be used. You are free to refuse our request for this information, with the understanding that we may be unable to provide you with some of your desired services without it.</p>
+<h2 class="subtitle">Use of information</h2>
+<p>We may use a combination of identifying and non-identifying information to understand who our visitors are, how they use our services, and how we may improve their experience of our website in future. We also use this information to gain insights into demographics, product and service popularity, and the effectiveness of our marketing activity. We do not disclose the specifics of this information publicly, but may share aggregated and anonymised versions of this information, for example, in website and customer usage trend reports.
+We primarily use your personal information for the purposes of granting you access to engage with certain areas of our site, to charge you for your subscription, and to notify you of any changes to your account and service.
+We may use your personal details to contact you with updates about our website and services, along with promotional content that we believe may be of interest to you. If you wish to opt out of receiving promotional content, you can follow the “unsubscribe” instructions provided alongside any promotional correspondence from us.</p><h2 class="subtitle">Data processing and storage</h2>
+The personal information we collect is stored and processed in European Union, or where we or our partners, affiliates and third-party providers maintain facilities. We only transfer data within jurisdictions subject to data protection laws that reflect our commitment to protecting the privacy of our users.
+We only retain personal information and business data for as long as necessary to provide a service, or to improve our services in future. While we retain this data, we will protect it within commercially acceptable means to prevent loss and theft, as well as unauthorised access, disclosure, copying, use or modification. That said, we advise that no method of electronic transmission or storage is 100% secure, and cannot guarantee absolute data security.
+If you request your personal information be deleted, or where your personal information becomes no longer relevant to our operations, we will erase it from our system within a reasonable timeframe.</p>
+<h2 class="subtitle">Cookies</h2>
+<p>We use “cookies” to collect information about you and your activity across our site. A cookie is a small piece of data that our website stores on your computer, and accesses each time you visit, so we can understand how you use our site. This helps us serve you content based on preferences you have specified. Please refer to our Cookie Policy for more information.</p>
+<h2 class="subtitle">Third-party access to information</h2>
+<p>We may use third-party services for:
+<ul>
+<li>Analytics tracking</li>
+<li>User authentication</li>
+<li>Advertising and promotion</li>
+<li>Content marketing</li>
+<li>Email marketing</li>
+<li>Payment processing</li>
+</ul>
+</p>
+<p>services may access our data solely for the purpose of performing specific tasks on our behalf. We do not share any personally identifying information with them without your explicit consent. We do not give them permission to disclose or use any of our data for any other purpose.
+We may, from time to time, allow limited access to our data by external consultants and agencies for the purpose of analysis and service improvement. This access is only permitted for as long as necessary to perform a specific function. We only work with external agencies whose privacy policies align with ours.
+We will refuse government and law enforcement requests for data if we believe a request is too broad or unrelated to its stated purpose. However, we may cooperate if we believe the requested information is necessary and appropriate to comply with legal process, to protect our own rights and property, to protect the safety of the public and any person, to prevent a crime, or to prevent what we reasonably believe to be illegal, legally actionable, or unethical activity.
+We do not otherwise share or supply personal information to third parties. We do not sell or rent your personal information to marketers or third parties.</p>
+<h2 class="subtitle">Limits of our policy</h2>
+<p>This privacy policy only covers Storeed's own collecting and handling of data. We only work with partners, affiliates and third-party providers whose privacy policies align with ours, however we cannot accept responsibility or liability for their respective privacy practices.
+Our website may link to external sites that are not operated by us. Please be aware that we have no control over the content and policies of those sites, and cannot accept responsibility or liability for their respective privacy practices.</p>
+<h2 class="subtitle">Changes to this policy</h2>
+<p>At our discretion, we may change our privacy policy to reflect current acceptable practices. We will take reasonable steps to let users know about changes via our website. If you are a registered user on https://storeed.me, we will notify you using the contact details saved in your account. Your continued use of this site after any changes to this policy will be regarded as acceptance of our practices around privacy and personal information. </p>
+<h2 class="subtitle">Your rights and responsibilities</h2>
+<p>As our user, you have the right to be informed about how your data is collected and used. You are entitled to know what data we collect about you, and how it is processed. You are entitled to correct and update any personal information about you, and to request this information be deleted. You may amend or remove your account information at any time, using the tools provided in your account control panel or with an email request to contact@storeed.me.
+You are entitled to restrict or object to our use of your data, while retaining the right to use your personal information for your own purposes. You have the right to opt out of data about you being used in decisions based solely on automated processing.
+Feel free to contact@storeed.me if you have any concerns or questions about how we handle your data and personal information.</p>
+<p>Storeed Data Controller : Hervé Rincent (contact@storeed.me)
+Storeed Data Protection Officer : Hervé Rincent (contact@storeed.me)</p>
+
+<h1 class="title">Cookie Policy</h1>
+<p>We use cookies to help improve your experience of https://storeed.me. This cookie policy is part of Storeed's privacy policy, and covers the use of cookies between your device and our site. We also provide basic information on third-party services we may use, who may also use cookies as part of their service, though they are not covered by our policy.
+If you don’t wish to accept cookies from us, you should instruct your browser to refuse cookies from https://storeed.me, with the understanding that we may be unable to provide you with some of your desired content and services.</p>
+<h2 class="subtitle">What is a cookie?</h2>
+<p>A cookie is a small piece of data that a website stores on your device when you visit, typically containing information about the website itself, a unique identifier that allows the site to recognise your web browser when you return, additional data that serves the purpose of the cookie, and the lifespan of the cookie itself.
+Cookies are used to enable certain features (eg. logging in), to track site usage (eg. analytics), to store your user settings (eg. timezone, notification preferences), and to personalise your content (eg. advertising, language).
+Cookies set by the website you are visiting are normally referred to as “first-party cookies”, and typically only track your activity on that particular site. Cookies set by other sites and companies (ie. third parties) are called “third-party cookies”, and can be used to track you on other websites that use the same third-party service.</p>
+<h2 class="subtitle">Types of cookies and how we use them</h2>
+<p><b>Essential cookies</b></p>
+<p>Essential cookies are crucial to your experience of a website, enabling core features like user logins, account management, shopping carts and payment processing. We use essential cookies to enable certain functions on our website.</p>
+<p><b>Performance cookies</b></p>
+<p>Performance cookies are used in the tracking of how you use a website during your visit, without collecting personal information about you. Typically, this information is anonymous and aggregated with information tracked across all site users, to help companies understand visitor usage patterns, identify and diagnose problems or errors their users may encounter, and make better strategic decisions in improving their audience’s overall website experience. These cookies may be set by the website you’re visiting (first-party) or by third-party services. We use performance cookies on our site.</p>
+<p><b>Functionality cookies</b></p>
+<p>Functionality cookies are used in collecting information about your device and any settings you may configure on the website you’re visiting (like language and timezone settings). With this information, websites can provide you with customised, enhanced or optimised content and services. These cookies may be set by the website you’re visiting (first-party) or by third-party service. We use functionality cookies for selected features on our site.</p>
+<p><b>Targeting/advertising cookies</b></p>
+<p>Targeting/advertising cookies are used in determining what promotional content is more relevant and appropriate to you and your interests. Websites may use them to deliver targeted advertising or to limit the number of times you see an advertisement. This helps companies improve the effectiveness of their campaigns and the quality of content presented to you. These cookies may be set by the website you’re visiting (first-party) or by third-party services. Targeting/advertising cookies set by third-parties may be used to track you on other websites that use the same third-party service. We do not use this type of cookie on our site.</p>
+<h2 class="subtitle">Third-party cookies on our site</h2>
+<p>We may employ third-party companies and individuals on our websites—for example, analytics providers and content partners. We grant these third parties access to selected information to perform specific tasks on our behalf. They may also set third-party cookies in order to deliver the services they are providing. Third-party cookies can be used to track you on other websites that use the same third-party service. As we have no control over third-party cookies, they are not covered by Storeed's cookie policy.</p>
+<h2 class="subtitle">Our third-party privacy promise</h2>
+<p>We review the privacy policies of all our third-party providers before enlisting their services to ensure their practices align with ours. We will never knowingly include third-party services that compromise or violate the privacy of our users.</p>
+<h2 class="subtitle">How you can control or opt out of cookies</h2>
+<p>
+If you do not wish to accept cookies from us, you can instruct your browser to refuse cookies from our website. Most browsers are configured to accept cookies by default, but you can update these settings to either refuse cookies altogether, or to notify you when a website is trying to set or update a cookie.
+If you browse websites from multiple devices, you may need to update your settings on each individual device.
+Although some cookies can be blocked with little impact on your experience of a website, blocking all cookies may mean you are unable to access certain features and content across the sites you visit.
+</p>
+
+        </div>
+    </div>
+    <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    props: {
+        'isVisible': {
+        type : Boolean,
+        default: false
+      }
+    }
+}
+</script>
+
